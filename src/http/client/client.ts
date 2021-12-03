7ca1ec07175cc01e452e28a1fe49a0e6879b1c32
Pablo Ibanez <me@pabloibanez.com>
2021-12-03 21:41:50 +0000

    client removed

--- src/http/client/client.ts
+++
@@ -1,6 +0,0 @@
-import { Client } from '@autonomo/common';
-import axios from '../axios';
-
-export const addClient = (client: Client) => {
-  return axios.post('/client', client);
-};
--- src/http/index.ts
+++ src/http/index.ts
@@ -1,11 +1,7 @@
-import { addClient } from './client/client';
 import { addInvoice, getInvoices } from './invoice/invoice';
 import { getUser } from './user/user';
 
 export default {
-  client: {
-    addClient
-  },
   invoice: {
     addInvoice,
     getInvoices
